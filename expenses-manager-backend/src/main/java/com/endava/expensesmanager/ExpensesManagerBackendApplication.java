7becc24c7652afe94fd555b81ee7d2d33a398327
Dragos Dan Dordea <Dragos.Dordea@endava.com>
2024-08-01 12:05:32 +0300

    Updating

--- expenses-manager-backend/src/main/java/com/endava/expensesmanager/ExpensesManagerBackendApplication.java
+++ expenses-manager-backend/src/main/java/com/endava/expensesmanager/ExpensesManagerBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpensesManagerBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpensesManagerBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesManagerBackendApplication.class, args);
+    }
 
 }
