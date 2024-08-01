809b2a70b3d678d3160f6734349bd6b62e205a07
Dragos Dan Dordea <Dragos.Dordea@endava.com>
2024-08-01 11:51:51 +0300

    Initial commit

---
+++ expenses-manager-backend/src/main/java/com/endava/expensesmanager/ExpensesManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.endava.expensesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesManagerBackendApplication.class, args);
+	}
+
+}
---
+++ expenses-manager-backend/src/test/java/com/endava/expensesmanager/ExpensesManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.expensesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagerBackendApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
