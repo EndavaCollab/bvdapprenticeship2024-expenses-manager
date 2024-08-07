6212525e4cf23269c84a0a0288289c2c0f60244e
ProtCosmin <147825426+ProtCosmin@users.noreply.github.com>
2024-08-07 13:25:54 +0300

    Created left side bar

--- expenses-manager-frontend/src/main/frontend/src/app/app.module.ts
+++ expenses-manager-frontend/src/main/frontend/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import {AppComponent} from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { ReportsComponent } from './reports/reports.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        SidebarComponent,
+        HomeComponent,
+        ExpensesComponent,
+        ReportsComponent,
+        LoginComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
