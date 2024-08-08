59b348e1b421116dd9a68a06a28c6bf971eac096
alinsarbu23 <alin.sarbu@student.unitbv.ro>
2024-08-08 14:44:03 +0300

    Create in database predefined categories

---
+++ expenses-manager-backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description, color) VALUES ('Food & Restaurants', '#ff6347');
+INSERT INTO category (description, color) VALUES ('Car', '#1e90ff');
+INSERT INTO category (description, color) VALUES ('Subscriptions', '#8a2be2');
+INSERT INTO category (description, color) VALUES ('Entertainment', '#ff4500');
+INSERT INTO category (description, color) VALUES ('Education', '#3cb371');
+INSERT INTO category (description, color) VALUES ('Saving', '#ffd700');
+INSERT INTO category (description, color) VALUES ('Clothing', '#ff69b4');
+INSERT INTO category (description, color) VALUES ('Housing', '#20b2aa');
+INSERT INTO category (description, color) VALUES ('Health', '#db7093');
