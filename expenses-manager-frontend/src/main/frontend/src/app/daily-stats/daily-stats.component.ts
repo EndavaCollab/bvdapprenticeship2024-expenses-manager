d1ae74a888034eb77c5aa9038784f49c9d7db53f
alinsarbu23 <alin.sarbu@student.unitbv.ro>
2024-08-30 22:14:34 +0300

    Applied changes

--- expenses-manager-frontend/src/main/frontend/src/app/daily-stats/daily-stats.component.ts
+++ expenses-manager-frontend/src/main/frontend/src/app/daily-stats/daily-stats.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
 
+interface Category {
+  id: number;
+  name: string;
+  colorCode: string;
+}
+
 @Component({
   selector: 'app-daily-stats',
   templateUrl: './daily-stats.component.html',
@@ -8,23 +14,28 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
 })
 export class DailyStatsComponent implements OnInit {
 
-  data: any[] = [
-    { name: 'Food & Restaurants', value: 30 },
-    { name: 'Car', value: 25 },
-    { name: 'Subscriptions', value: 20 },
-    { name: 'Entertainment', value: 15 },
-    { name: 'Coffee', value: 10 }
+  categories: Category[] = [
+    { id: 1, name: 'Food & Restaurants', colorCode: '#46B1D9' },
+    { id: 2, name: 'Car', colorCode: '#5844E3' },
+    { id: 3, name: 'Subscriptions', colorCode: '#E047AB' },
+    { id: 4, name: 'Entertainment', colorCode: '#F78486' },
+    { id: 5, name: 'Coffee', colorCode: '#A1D12E' }
   ];
+
+  data = this.categories.map(category => ({ name: category.name, value: this.getRandomValue() }));
 
   colorScheme: Color = {
     name: 'custom',
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: ['#46B1D9', '#5844E3', '#E047AB', '#F78486', '#A1D12E']
+    domain: this.categories.map(category => category.colorCode)
   };
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  getRandomValue(): number { //for every category
+    return Math.floor(Math.random() * 100); 
   }
 }
